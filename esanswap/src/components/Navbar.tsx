ca867a722a20a492019aa7e38722325fb9b45568
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-24 14:55:03 +0100

    ..

--- esanswap/src/components/Navbar.tsx
+++ esanswap/src/components/Navbar.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 import { Logo } from "../../public/assets";
 import { ModeToggle } from "@/components/ModeToggle";
+import { MintHoverCard } from "./MintHoverCard";
+import { DeveloperHoverCard } from "./DeveloperHoverCard";
 import { WalletDropdown } from "./WalletDropdown";
 import { ConnectWallet } from "./ConnectWallet";
 import { lusitana, encodeSansExpanded } from "@/fonts/font";
@@ -12,36 +16,44 @@ const navbarArr = [
   { id: 1, title: "Swap", link: "/" },
   { id: 2, title: "Stake", link: "/pages/stake" },
   { id: 3, title: "Liquidity", link: "/pages/liquidity" },
-  { id: 4, title: "Minting" },
-  { id: 5, title: "Developer" },
+  // { id: 4, title: "Minting" },
+  // { id: 5, title: "Developer" },
 ];
 
 export default function Navbar() {
+  const [hoveredItem, setHoveredItem] = useState<number | null>(null);
+
   return (
     <nav
       className={`flex mx-[30px] my-[20px] fixed text-[#af7383] dark:text-white ${encodeSansExpanded.className}`}
     >
       <Link href="/" className="flex">
         <div className="flex">
-          <Image src={Logo} alt="logo" />
-          <h3 className="my-3 mx-1 pr-2 text-[20px] font-bold">EsanSwap</h3>
+          <Image src={Logo} alt="logo" className="lg:mt-2 xl:flex" />
+          <h3 className="my-3 xl:my-4 lg:hidden xl:flex sm:flex mx-1 pr-2 text-[20px] font-bold">
+            EsanSwap
+          </h3>
         </div>
       </Link>
-      {navbarArr.map((item) => (
-        <Link
-          key={item.id}
-          href={item.link || "/"}
-          className="my-3 mx-4 font-medium hover:bg-[#e2e8f0] dark:hover:bg-[#334155] p-1 rounded-lg"
-        >
-          {item.title}
-        </Link>
-      ))}
-      <div className="my-2 ml-[240px] flex">
+      <div className="relative mt-3 hidden lg:flex">
+        {navbarArr.map((item) => (
+          <Link
+            key={item.id}
+            href={item.link || ""}
+            className="my-3 mx-4 lg:my-1 font-medium underline-offset-4 hover:underline p-1 pb-0"
+          >
+            {item.title}
+          </Link>
+        ))}
+        <MintHoverCard />
+        <DeveloperHoverCard />
+      </div>
+      <div className="my-2 ml-[320px] md11:ml-[350px] lg:ml-[220px] xl:ml-[340px] flex">
         <WalletDropdown />
         <div className="mx-2">
           <ConnectWallet />
         </div>
-        <div className="mx-2">
+        <div className="mx-2 hidden md:flex">
           <ModeToggle />
         </div>
       </div>
